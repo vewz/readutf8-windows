cb845632f5769301748beaa6787deb270384215a
影桜 <79582228+vewz@users.noreply.github.com>
2024-04-26 12:04:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,15 +2,17 @@
 #include <windows.h>
 
 int main(void) {
-  wchar_t buffer[80];
+  WCHAR buffer[80];
   char utf8_decoded[80];
   DWORD read_len;
   int decoded_len;
 
-  memset(buffer, 80, sizeof(wchar_t));
+  memset(buffer, 80, sizeof(WCHAR));
   memset(utf8_decoded, 80, sizeof(char));
 
   printf("入力>>");
+  fflush(stdout);
+
   if (!ReadConsoleW(GetStdHandle(STD_INPUT_HANDLE), buffer, 80, &read_len, NULL)) {
     printf("Failed to read stdin\n");
     return 1;
